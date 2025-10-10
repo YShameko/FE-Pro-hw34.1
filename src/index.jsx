adfcc708e9aa99fd4b5258a3c4fb611da9bc8495
YShameko <yshameko@gmail.com>
2025-10-10 19:02:54 +0300

    hw34.1

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import './style.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from './App.jsx';
+
+const rootEl = document.getElementById('main');
+const root = ReactDOM.createRoot(rootEl);
+
+root.render(<App />);
+
+// ============================================================================================================
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CssPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.jsx',
+
+    output: {
+        path: path.resolve(__dirname, 'client'),
+        filename: 'bundle[fullhash].js',
+        clean: true, 
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+
+    mode: 'development',
+
+    plugins: [
+        new HtmlPlugin({
+            template: './src/index.html'
+        }),
+        // new CopyWebpackPlugin({
+        //     patterns: [
+        //         { from: './src/style.css', to: './'}
+        //     ]
+        // }),
+        new CssPlugin({
+            filename: 'style[fullhash].css'
+        }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        "presets": [
+                            ["@babel/preset-env"],
+                            ["@babel/preset-react"]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/, 
+                use: [
+                    CssPlugin.loader, 
+                    'css-loader'
+                ],
+            },
+        ]
+    },
+
+    devServer: {
+        port: 5000,
+        static: {
+            directory: path.join(__dirname, 'client')
+        },
+        devMiddleware: {
+            writeToDisk: true
+        },
+        open: true
+    }
+}
