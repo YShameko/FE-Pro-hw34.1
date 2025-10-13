a1e1328c1716fe8a8853d22a7681831fb50cbb5c
YShameko <yshameko@gmail.com>
2025-10-13 17:16:48 +0300

    Updates

---
+++ main.2b0ed250d0b3cd5d9213.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdate"]("main",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("89e942f5b83f77479150")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=main.2b0ed250d0b3cd5d9213.hot-update.js.map
